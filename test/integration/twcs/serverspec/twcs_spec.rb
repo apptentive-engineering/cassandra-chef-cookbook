dc38765f8c7b0ca4fd6cc238d4b0afb0b2dd9e76
Andres Rojas <andres@conpat.io>
2017-05-19 17:15:52 -0700

    Clean up the ::twcs tests

--- test/integration/twcs/serverspec/twcs_spec.rb
+++ test/integration/twcs/serverspec/twcs_spec.rb
@@ -1,23 +1,8 @@
 require 'spec_helper'
 
-describe package('git') do
-  it { should be_installed }
-end
-
-describe file('/tmp/twcs-repo') do
-  it { should be_directory }
-end
-
-describe file('/usr/local/maven') do
-  it { should be_symlink }
-end
-
-describe file('/usr/share/cassandra/lib') do
-  it { should be_directory }
-end
-
 describe file('/usr/share/cassandra/lib/TimeWindowCompactionStrategy-2.2.5.jar') do
   it { should be_file }
   it { should be_owned_by 'vagrant' }
   it { should be_grouped_into 'vagrant' }
+  it { should be_mode 755 }
 end
